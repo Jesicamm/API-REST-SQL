16cf2b777c70bc0a8b70400dfbe16d9b95ea312e
keika <jesicamorocho.m@gmail.com>
2021-02-28 19:33:59 +0100

    adding user login

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,5 +8,19 @@ router.post('/register', async(req, res) => {
     res.json(user);
 });
 
+router.post('/login', async(req, res) => {
+    const user = await User.findOne({ where: { email: req.body.email } });
+    if (user) {
+        const equal = bcrypt.compareSync(req.body.password, user.password);
+        if (equal) {
+            res.json({ succes: 'todo ok' });
+        } else {
+            res.json({ error: "error en usuario y/o contraseña" });
+        }
+    } else {
+        res.json({ error: 'Error en usuario y/o contraseña' });
+    }
+});
+
 
 module.exports = router;
